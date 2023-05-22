741a10e281736173e7452c47f82c33c65b8e12cf
Vicky03407 <vicky053407@gmail.com>
2023-05-22 23:27:52 +0530

    initial commit

---
+++ CRUD/backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = 5000;
+
+const app = express();
+
+app.listen(port, ()=>{
+    console.log(`server started on port ${port}`)
+})
